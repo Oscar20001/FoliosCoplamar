677e9493a223c777b572c69f5b88f14b42b2b61a
Oscar20001 <oscaralexander2000@gmail.com>
2026-05-16 17:58:47 -0600

    Fijar credenciales para Netlify

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,15 +3,15 @@ import { getAuth, signInAnonymously } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyBjoidd5OWCRLEJx3fc1qVEA8Yv_9CP3QM",
+  authDomain: "folios-imss.firebaseapp.com",
+  projectId: "folios-imss",
+  storageBucket: "folios-imss.firebasestorage.app",
+  messagingSenderId: "503603285610",
+  appId: "1:503603285610:web:eb0b738b6814f3b372c413"
 };
 
-const isFirebaseConfigured = firebaseConfig.apiKey && firebaseConfig.apiKey !== 'PEGA_AQUI_TU_API_KEY';
+const isFirebaseConfigured = true;
 
 let app = null;
 let auth = null;
